fa33947a90f324ba469e330a27a69983b8e9e0a2
Mohamed Belgaied Hassine <belgaied2@hotmail.com>
2021-03-17 00:56:51 +0100

    First commit working Harvester Install

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_uuid" {
+  description = "Project UUID for Equinix BareMetal"
+  type = string
+
+}
+
+variable "auth_token" {
+  description = "API Token for programmatically accessing Equinix Metal"
+  type = string
+  
+}
