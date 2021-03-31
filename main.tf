0d8a2ebbffd9edd7bb1c9f8f032d7d9e41da3bb1
Mohamed Belgaied Hassine <belgaied2@hotmail.com>
2021-03-31 09:57:24 +0200

    changed type of instance to spot market

--- main.tf
+++ main.tf
@@ -21,16 +21,43 @@ data "metal_project_ssh_key" "project_key" {
 
 
 # Create a device and add it to tf_project_1
-resource "metal_device" "harvester1" {
-  hostname         = "harvester-1"
-  plan             = "c3.small.x86"
-  facilities       = ["ny5"]
-  operating_system = "custom_ipxe"
-  billing_cycle    = "hourly"
-  project_id       = local.project_id
-  ipxe_script_url  = "https://raw.githubusercontent.com/belgaied2/hvst-ipxe/main/equinix/ipxe-create"
-  always_pxe       = "false" 
-  project_ssh_key_ids = [ data.metal_project_ssh_key.project_key.id ]
-  # if you have created project with metal_project resource, refer to its ID
-  # project_id       = metal_project.cool_project.id
+# resource "metal_device" "harvester1" {
+#   hostname         = "harvester-1"
+#   plan             = "c3.small.x86"
+#   facilities       = ["sv15"]
+#   operating_system = "custom_ipxe"
+#   billing_cycle    = "hourly"
+#   project_id       = local.project_id
+#   ipxe_script_url  = "https://raw.githubusercontent.com/belgaied2/hvst-ipxe/main/equinix/ipxe-create"
+#   always_pxe       = "false" 
+#   project_ssh_key_ids = [ data.metal_project_ssh_key.project_key.id ]
+#   # if you have created project with metal_project resource, refer to its ID
+#   # project_id       = metal_project.cool_project.id
+# }
+
+# Create a spot market device add it to tf_project_1
+resource "metal_spot_market_request" "req" {
+  project_id    = local.project_id
+  max_bid_price = 0.70
+  facilities    = ["sv15"]
+  devices_min   = 1
+  devices_max   = 1
+
+  instance_parameters {
+    # hostname         = "testspot"
+    # billing_cycle    = "hourly"
+    # operating_system = "coreos_stable"
+    # plan             = "t1.small.x86"
+    hostname         = "harvester-1"
+    plan             = "m3.large.x86"
+    # facilities       = ["sv15"]
+    operating_system = "custom_ipxe"
+    billing_cycle    = "hourly"
+    # project_id       = local.project_id
+    ipxe_script_url  = "https://raw.githubusercontent.com/belgaied2/hvst-ipxe/main/equinix/ipxe-create"
+    always_pxe       = "false" 
+    # project_ssh_key_ids = [ data.metal_project_ssh_key.project_key.id ]
+    # if you have created project with metal_project resource, refer to its ID
+    # project_id       = metal_project.cool_project.id
+  }
 }
